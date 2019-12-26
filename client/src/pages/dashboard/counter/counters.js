997abbdca70fc44eebc66fee98dc5baaec16dea8
azhar <azharulislamnipu@gmail.com>
2019-12-26 21:32:15 +0600

    some changes  Updated

--- client/src/pages/dashboard/counter/counters.js
+++ client/src/pages/dashboard/counter/counters.js
@@ -5,7 +5,6 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col';
 import {connect} from 'react-redux';
 import {  loadCounters, removeCounter } from '../../../store/actions/counterActions';
-import { addFlashMessage, deleteFlashMessage } from '../../../store/actions/flashMessages';
 import { Link } from 'react-router-dom'
  class Counters extends Component {
 
@@ -83,7 +82,7 @@ import { Link } from 'react-router-dom'
                                             <td>
                                                 <div>
                                                     <a href="#" class="btn btn-primary btn-sm mr-2">Edit</a>
-                                                    <button className='btn btn-danger btn-sm ml-2' onClick={ ()=> { this.props.deleteFlashMessage(counter._id)}} >Delete</button>
+                                                    <button className='btn btn-danger btn-sm ml-2' onClick={ ()=> { this.props.removeCounter(counter._id)}} >Delete</button>
                                                 </div>
                                             </td>
                                         </tr>
@@ -115,4 +114,4 @@ const mapStateToProps = state => ({
     counters: state.counter,
 })
 
-export default connect(mapStateToProps, { loadCounters, removeCounter , deleteFlashMessage})(Counters)
+export default connect(mapStateToProps, { loadCounters, removeCounter })(Counters)
